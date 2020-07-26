ec1fc08449b598d4184545d10bfe46c72e160ec5
prathmeshthorat <58651263+prathmeshthorat@users.noreply.github.com>
2020-07-26 19:36:18 +0530

    Added inline comments

--- pygame_one.py
+++ pygame_one.py
@@ -1,8 +1,12 @@
 import pygame 
 
+# Initialise the instance 
 pygame.init()
+# Surface dimentions
 gameDisplay=pygame.display.set_mode((800,600))
+# Name
 pygame.display.set_caption('HA HA')
+# Add clock
 clock= pygame.time.Clock()
 crashed = False
 
@@ -13,7 +17,9 @@ while not crashed:
         
         print (event)
         
+    # pygame.display.update() OR pygame.display.flip()
     pygame.display.update()
+    # fps
     clock.tick(60)
     
 pygame.quit()
